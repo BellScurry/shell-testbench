c4f3d720d6cd5e61a2e76112f662325f19faca17
Jongho Kim <scurry2119@gmail.com>
2016-05-07 16:35:49 +0900

    Good IFS description and for loop example

--- ifs.sh
+++ ifs.sh
@@ -5,12 +5,19 @@
 #
 #
 
-var1="1:2:3:4:5"
+var1="a:b:c:d:e"
 
 # Do not split this line.
 IFS=":" read -ra array <<< $var1
 
 for i in {0..4}
 do
+    echo ${array[$i]}
+done
+
+array_length=${#array[@]}
+echo "Array Length = $array_length"
+
+for ((i=0 ; i < $array_length ; i++ )); do
     echo ${array[$i]}
 done
