fd242bcdd4bec88d369cd507647566e984953695
Jongho Kim <scurry2119@gmail.com>
2016-07-14 10:25:12 -0400

    Improve Readibility

--- project2.sh
+++ project2.sh
@@ -2,25 +2,20 @@
 ###   Usage: yscec_unzip <zip file from YSCEC> <directory to which you unzip>
 yscec_unzip () {
 
-    ###   We need $1 and $2, so $# must be 2.
     if [ "$#" -ne 2 ]; then
         echo "wrong number of parameters were passed to function yscec_unzip()"
         echo "Usage: yscec_unzip <zip file from YSCEC> <directory to which you unzip>"
         exit -1
     fi
-
-    #ascii_zipfile=$(echo $1 | perl -pe 's/[^[:ascii:]]//g')
-    #mv "$1" $ascii_zipfile
     
     mkdir $2
-    unzip $1 -d $2
+    unzip "$1" -d $2
 }
 
 ###   Trim all prefix attatched by YSCEC in filenames.
 ###   Usage: yscec_remove_prefix_in <target directory>
 yscec_remove_prefix_in () {
     
-    ###   We need $1, so $# must be 1.
     if [ "$#" -ne 1 ]; then
         echo "wrong number of parameters were passed to function yscec_remove_prefix_in()"
         echo "Usage: yscec_remove_prefix_in <target directory>"
@@ -44,5 +39,16 @@ yscec_remove_prefix_in () {
     done
 }
 
-yscec_unzip $1 "submission"
-yscec_remove_prefix_in "submission"
+yscec_extract () {
+
+    if [ "$#" -ne 2 ]; then
+        echo "wrong number of parameters were passed to function yscec_extract()"
+        echo "Usage: yscec_unzip <zip file from YSCEC> <directory to which you unzip>"
+        exit -1
+    fi
+
+    yscec_unzip "$1" $2
+    yscec_remove_prefix_in $2
+}
+
+yscec_extract $1 "submission"
