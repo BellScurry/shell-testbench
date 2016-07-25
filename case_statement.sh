61ed435b4f8f810a00963683a789400adc37a262
Jongho Kim <scurry2119@gmail.com>
2016-07-24 22:39:49 -0400

    The case statement in shell is different from C

---
+++ case_statement.sh
@@ -0,0 +1,14 @@
+case $1 in
+    hel*)
+        echo "hello"
+        ;;
+    Hello)
+        echo "Also Hello!"
+        ;;
+    Hel*)
+        echo "Hello"
+        ;;
+    hello)
+        echo "also hello!"
+        ;;
+esac
